40a007f7310749a5ec0e0b921346c83764cc3535
h3athen <gurungaryan73@gmail.com>
2024-12-04 10:38:07 -0800

    Day 3 part 2

--- 2024/Day-3/solve.py
+++ 2024/Day-3/solve.py
@@ -9,12 +9,28 @@ def part_one(line):
         sum += x*y
     print("Part 1:",sum)
 
+def part_two(line):
+    enable = True
+    sum=0
+    matches = re.findall("mul\\(\\d+\\,\\d+\\)|don't\\(\\)|do\\(\\)",line)
+    for match in matches:
+        if match == "do()":
+            enable = True
+        elif match == "don't()":
+            enable = False
+        else:
+            if enable:
+                digits = (re.findall("\\d+",match))
+                x,y = map(int, digits)
+                sum += x*y
+    print("Part 2:",sum)
 
 def main():
     with open('input.txt', 'r') as input:
         line = input.read().strip()
 
     part_one(line)
+    part_two(line)
 
 if __name__ == "__main__":
     main()
